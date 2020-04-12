a3093cb303a27d2667b8df67a1ea00e2b279707a
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-04-12 12:37:19 +0400

    commit 3.3

--- app/store/category/types.ts
+++ app/store/category/types.ts
@@ -1,11 +1,19 @@
 export interface CategoryState {
   loading: boolean;
+  categories: string[] | null;
 }
+
+export const GET_CATEGORIES = 'GET_CATEGORIES';
 
 export const START_FETCHING_CATEGORIES = 'START_FETCHING_CATEGORIES';
 
-// interface StartFetchingCategoryAction {
-//   type: typeof START_FETCHING_CATEGORIES;
-// }
+interface StartFetchingCategoryAction {
+  type: typeof START_FETCHING_CATEGORIES;
+}
 
-// export type CategoryActionTypes = StartFetchingCategoryAction;
+export interface CategoryActionTypes {
+  type: string;
+  payload: string[];
+}
+
+export type CategoryTypes = CategoryActionTypes | StartFetchingCategoryAction;
